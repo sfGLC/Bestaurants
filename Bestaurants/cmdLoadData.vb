46993d87c549afba9a673739ba3a1f7ef5dfaeea
sfGLC <sfan@glc.org>
2016-10-31 13:17:42 -0400

    Ep1

---
+++ Bestaurants/cmdLoadData.vb
@@ -0,0 +1,110 @@
+Imports System.Runtime.InteropServices
+Imports System.Drawing
+Imports ESRI.ArcGIS.ADF.BaseClasses
+Imports ESRI.ArcGIS.ADF.CATIDs
+Imports ESRI.ArcGIS.Framework
+Imports ESRI.ArcGIS.ArcMapUI
+
+<ComClass(cmdLoadData.ClassId, cmdLoadData.InterfaceId, cmdLoadData.EventsId), _
+ ProgId("Bestaurants.cmdLoadData")> _
+Public NotInheritable Class cmdLoadData
+    Inherits BaseCommand
+
+#Region "COM GUIDs"
+    ' These  GUIDs provide the COM identity for this class 
+    ' and its COM interfaces. If you change them, existing 
+    ' clients will no longer be able to access the class.
+    Public Const ClassId As String = "bdf78d37-529c-4605-9297-084a469a7887"
+    Public Const InterfaceId As String = "ac251a01-5431-4ca6-8b4f-0d3eedd820ea"
+    Public Const EventsId As String = "b11c209e-bf4b-4275-a8b8-3c2ccf1f1def"
+#End Region
+
+#Region "COM Registration Function(s)"
+    <ComRegisterFunction(), ComVisibleAttribute(False)> _
+    Public Shared Sub RegisterFunction(ByVal registerType As Type)
+        ' Required for ArcGIS Component Category Registrar support
+        ArcGISCategoryRegistration(registerType)
+
+        'Add any COM registration code after the ArcGISCategoryRegistration() call
+
+    End Sub
+
+    <ComUnregisterFunction(), ComVisibleAttribute(False)> _
+    Public Shared Sub UnregisterFunction(ByVal registerType As Type)
+        ' Required for ArcGIS Component Category Registrar support
+        ArcGISCategoryUnregistration(registerType)
+
+        'Add any COM unregistration code after the ArcGISCategoryUnregistration() call
+
+    End Sub
+
+#Region "ArcGIS Component Category Registrar generated code"
+    Private Shared Sub ArcGISCategoryRegistration(ByVal registerType As Type)
+        Dim regKey As String = String.Format("HKEY_CLASSES_ROOT\CLSID\{{{0}}}", registerType.GUID)
+        MxCommands.Register(regKey)
+
+    End Sub
+    Private Shared Sub ArcGISCategoryUnregistration(ByVal registerType As Type)
+        Dim regKey As String = String.Format("HKEY_CLASSES_ROOT\CLSID\{{{0}}}", registerType.GUID)
+        MxCommands.Unregister(regKey)
+
+    End Sub
+
+#End Region
+#End Region
+
+
+    Private m_application As IApplication
+
+    ' A creatable COM class must have a Public Sub New() 
+    ' with no parameters, otherwise, the class will not be 
+    ' registered in the COM registry and cannot be created 
+    ' via CreateObject.
+    Public Sub New()
+        MyBase.New()
+
+        ' TODO: Define values for the public properties
+        MyBase.m_category = "Bestaurants_Tools"  'localizable text 
+        MyBase.m_caption = "Load Data"   'localizable text 
+        MyBase.m_message = "Load Data into ArcMap from External Files"   'localizable text 
+        MyBase.m_toolTip = "Load Data" 'localizable text 
+        MyBase.m_name = "Bestaurants_Tools_LoadData"  'unique id, non-localizable (e.g. "MyCategory_ArcMapCommand")
+
+        Try
+            'TODO: change bitmap name if necessary
+            Dim bitmapResourceName As String = Me.GetType().Name + ".bmp"
+            MyBase.m_bitmap = New Bitmap(Me.GetType(), bitmapResourceName)
+        Catch ex As Exception
+            System.Diagnostics.Trace.WriteLine(ex.Message, "Invalid Bitmap")
+        End Try
+
+
+    End Sub
+
+
+    Public Overrides Sub OnCreate(ByVal hook As Object)
+        If Not hook Is Nothing Then
+            m_application = CType(hook, IApplication)
+
+            'Disable if it is not ArcMap
+            If TypeOf hook Is IMxApplication Then
+                MyBase.m_enabled = True
+            Else
+                MyBase.m_enabled = False
+            End If
+        End If
+
+        ' TODO:  Add other initialization code
+    End Sub
+
+    Public Overrides Sub OnClick()
+        'TODO: Add cmdLoadData.OnClick implementation
+
+        MsgBox("Hello World!")
+
+        m_application.Caption = "Bestaurants application"
+    End Sub
+End Class
+
+
+
