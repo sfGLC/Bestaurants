e4f34c3414a29a2a43e2a7476088ee7ef5b98b58
sfGLC <sfan@glc.org>
2016-11-01 11:34:37 -0400

    Show&Hide

---
+++ Bestaurants/frmRestaurantsViewer.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRestaurantsViewer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmbLayers = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnShow = New System.Windows.Forms.Button()
+        Me.btnHide = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmbLayers
+        '
+        Me.cmbLayers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbLayers.FormattingEnabled = True
+        Me.cmbLayers.Location = New System.Drawing.Point(101, 40)
+        Me.cmbLayers.Name = "cmbLayers"
+        Me.cmbLayers.Size = New System.Drawing.Size(121, 21)
+        Me.cmbLayers.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Layers"
+        '
+        'btnShow
+        '
+        Me.btnShow.Location = New System.Drawing.Point(262, 40)
+        Me.btnShow.Name = "btnShow"
+        Me.btnShow.Size = New System.Drawing.Size(75, 23)
+        Me.btnShow.TabIndex = 2
+        Me.btnShow.Text = "Show"
+        Me.btnShow.UseVisualStyleBackColor = True
+        '
+        'btnHide
+        '
+        Me.btnHide.Location = New System.Drawing.Point(358, 40)
+        Me.btnHide.Name = "btnHide"
+        Me.btnHide.Size = New System.Drawing.Size(75, 23)
+        Me.btnHide.TabIndex = 3
+        Me.btnHide.Text = "Hide"
+        Me.btnHide.UseVisualStyleBackColor = True
+        '
+        'frmRestaurantsViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(523, 262)
+        Me.Controls.Add(Me.btnHide)
+        Me.Controls.Add(Me.btnShow)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbLayers)
+        Me.Name = "frmRestaurantsViewer"
+        Me.Text = "frmRestaurantsViewer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmbLayers As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnShow As System.Windows.Forms.Button
+    Friend WithEvents btnHide As System.Windows.Forms.Button
+End Class
---
+++ Bestaurants/frmRestaurantsViewer.vb
@@ -0,0 +1,60 @@
+﻿Imports ESRI.ArcGIS.Framework
+Imports ESRI.ArcGIS.Carto
+Imports ESRI.ArcGIS.ArcMapUI
+
+Public Class frmRestaurantsViewer
+
+    Private m_application As IApplication
+    Public Property ArcMapApplication() As IApplication
+        Get
+            Return m_application
+        End Get
+        Set(ByVal value As IApplication)
+            m_application = value
+        End Set
+    End Property
+
+
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        Dim pLayer As ILayer = getLayerByName(cmbLayers.SelectedItem.ToString)
+        pLayer.Visible = True
+        Dim pMxDoc As IMxDocument = m_application.Document
+        pMxDoc.ActivatedView.Refresh()
+        pMxDoc.UpdateContents()
+    End Sub
+
+    Private Sub btnHide_Click(sender As Object, e As EventArgs) Handles btnHide.Click
+        Dim pLayer As ILayer = getLayerByName(cmbLayers.SelectedItem.ToString)
+        pLayer.Visible = False
+        Dim pMxDoc As IMxDocument = m_application.Document
+        pMxDoc.UpdateContents()
+        pMxDoc.ActivatedView.Refresh()
+
+    End Sub
+    ''' <summary>
+    ''' Get the layer by name
+    ''' </summary>
+    ''' <param name="sLayerName"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function getLayerByName(sLayerName As String) As ILayer
+        Try
+
+            Dim pMxDoc As IMxDocument = m_application.Document
+            Dim pMap As IMap = pMxDoc.FocusMap
+            For i As Integer = 0 To pMap.LayerCount - 1
+                Dim pLayer As ILayer = pMap.Layer(i)
+
+                If pLayer.Name = sLayerName Then
+                    Return pLayer
+                End If
+            Next
+            Return Nothing
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+
+        End Try
+
+
+    End Function
+End Class
